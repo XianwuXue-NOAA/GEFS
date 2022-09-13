5eba2452a8a0a1d85b21ab03a62b46734a365cee
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2022-09-12 22:27:18 -0400

    Update HOMEGFS and Fix to port to Hera
     On branch feature/ops_port2hera
    	modified:   rocoto/parm/setbase
    	modified:   sorc/checkout.sh
    
    Refs: #19

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -13,9 +13,15 @@ fi
 echo Global_workflow checkout ...
 if [[ ! -d global-workflow.fd ]] ; then
     rm -f ${logs_dir}/checkout-global-workflow.log
-    git clone https://github.com/NOAA-EMC/global-workflow.git global-workflow.fd >>  ${logs_dir}/checkout-global-workflow.log 2>&1
-    cd global-workflow.fd
-    git checkout gefs_v12.3.0-0
+    if [[ -d /scratch1/NCEPDEV ]] ; then
+        git clone https://github.com/XianwuXue-NOAA/global-workflow.git global-workflow.fd >>  ${logs_dir}/checkout-global-workflow.log 2>&1
+        cd global-workflow.fd
+        git checkout release/gefs_v12_port2hera #gefs_v12.3.0-0
+    else
+        git clone https://github.com/NOAA-EMC/global-workflow.git global-workflow.fd >>  ${logs_dir}/checkout-global-workflow.log 2>&1
+        cd global-workflow.fd
+        git checkout gefs_v12.3.0-0
+    fi
 	cd sorc
     ./checkout.sh
     ERR=$?
