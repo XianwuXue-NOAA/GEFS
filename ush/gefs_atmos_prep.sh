cdbd945709987b1a06043260318b3ea496532b7c
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2023-01-18 18:28:05 -0500

    Change atmos_prep to read ratmanl.nc for ens mems
     On branch feature/gefs_v13_atmos_prep
     Changes to be committed:
    	modified:   jobs/JGEFS_ATMOS_PREP
    	modified:   ush/gefs_atmos_prep.sh
    
    Refs: #94

--- ush/gefs_atmos_prep.sh
+++ ush/gefs_atmos_prep.sh
@@ -13,208 +13,23 @@ export mem=$1
 export nmem=$(echo $mem|cut -c 2-)
 nmem=${nmem#0}
 
-export INPUT_TYPE="gaussian_netcdf"
-
-export FIXgfs=${FIXgfs:-$HOMEgfs/fix}
-
-export CRES=$(echo $CASE |cut -c2-5)
-CRES_H=$((CRES+CRES))
-export FIXfv3=$FIXgfs/orog/C$CRES
-export FIXfv3_H=$FIXgfs/orog/C$CRES_H
-export FIXsfc=$FIXfv3/fix_sfc
-export FIXam=${FIXam:-$FIXgfs/am}
-export VCOORD_FILE=${VCOORD_FILE:-$FIXam/global_hyblev.l${LEVS}.txt}
-
-if [[ $USE_EARLY_ENKF == YES ]]; then
-
-  if [[ $mem = c00 ]]; then
-    echo "Working on c00"
-    gmemdir=${COMINgdas}/atmos
-    memdir=${COMINgfs}/atmos
-  else
-    echo "Working on ${mem}"
-
-    (( cmem = nmem + memshift ))
-    if (( cmem > 80 )); then
-      (( cmem = cmem - 80 ))
-    fi
-    memchar="mem"$(printf %03i $cmem)
-
-    gmemdir=${COMINenkf}/${memchar}/atmos
-    memdir=${COMINenkfgfs}/${memchar}/atmos
-  fi
-
-  export INIDIR=$DATA
-  cd $INIDIR
-
-  mkdir -p $INIDIR/RESTART
-  if [[ -e $COMOUT ]]; then
-    rm -rf $COMOUT
-  fi
-  mkdir -p $COMOUT
-
-  CDATE=${PDY}${cyc}
-
-  if [[ "$DOIAU" = "YES" ]]; then
-    sCDATE=$($NDATE -3 $CDATE)
-    sPDY=$(echo $sCDATE | cut -c1-8)
-    scyc=$(echo $sCDATE | cut -c9-10)
-  else
-    sCDATE=$CDATE
-    sPDY=$PDY
-    scyc=$cyc
-  fi
-
-  gPDY=${pdyp}
-  gcyc=${cycp}
-
-  # Link all (except sfc_data) restart files from $gmemdir
-  for file in $(ls $gmemdir/RESTART/${sPDY}.${scyc}0000.*.nc); do
-    file2=$(echo $(basename $file))
-    file2=$(echo $file2 | cut -d. -f3-) # remove the date from file
-    fsuf=$(echo $file2 | cut -d. -f1)
-    if [ $fsuf != "sfc_data" ]; then
-      $NLN $file $INIDIR/RESTART/
-    fi
-  done
-
-  # Link sfcanl_data restart files from $memdir
-  for file in $(ls $memdir/RESTART/${sPDY}.${scyc}0000.*.nc); do
-    file2=$(echo $(basename $file))
-    file2=$(echo $file2 | cut -d. -f3-) # remove the date from file
-    fsufanl=$(echo $file2 | cut -d. -f1)
-    if [ $fsufanl = "sfcanl_data" ]; then
-      file2=$(echo $file2 | sed -e "s/sfcanl_data/sfc_data/g")
-      $NLN $file $INIDIR/RESTART/
-    fi
-  done
-
-  # Need a coupler.res when doing IAU
-  if [ $DOIAU = "YES" ]; then
-    rm -f $INIDIR/RESTART/${sPDY}.${scyc}0000.coupler.res
-    cat >> $INIDIR/RESTART/${sPDY}.${scyc}0000.coupler.res <<- EOF
-			2        (Calendar: no_calendar=0, thirty_day_months=1, julian=2, gregorian=3, noleap=4)
-			${gPDY:0:4}  ${gPDY:4:2}  ${gPDY:6:2}  ${gcyc}     0     0        Model start time:   year, month, day, hour, minute, second
-			${sPDY:0:4}  ${sPDY:4:2}  ${sPDY:6:2}  ${scyc}     0     0        Current model time: year, month, day, hour, minute, second
-		EOF
-  else
-    $NLN $gmemdir/RESTART/${sPDY}.${scyc}0000.coupler.res $INIDIR/RESTART/
-  fi
-
-  # Link increments
-  if [ $DOIAU = "YES" ]; then
-    for i in $(echo $IAUFHRS | sed "s/,/ /g" | rev); do
-      incfhr=$(printf %03i $i)
-      if [ $incfhr = "006" ]; then
-        increment_file=t${cyc}z.${PREFIX_ATMINC:-""}atminc.nc
-      else
-        increment_file=t${cyc}z.${PREFIX_ATMINC:-""}atmi${incfhr}.nc
-      fi
-      if [ ! -f $memdir/gfs.$increment_file ]; then
-        echo "ERROR: DOIAU = $DOIAU, but missing increment file for fhr $incfhr at $memdir/gfs.$increment_file"
-        echo "Abort!"
-        exit 1
-      fi
-      $NLN $memdir/gfs.$increment_file $INIDIR/gefs.$increment_file
-    done
-  else
-    increment_file=t${cyc}z.${PREFIX_ATMINC:-""}atminc.nc
-    if [ -f $memdir/gfs.$increment_file ]; then
-      $NLN $memdir/gfs.$increment_file $INIDIR/gefs.$increment_file
-    fi
-  fi
-
-  if [[ $mem = c00 ]]; then
-    CRES_INPUT=$CRES_H
-    FIXfv3_INPUT=$FIXfv3_H
-  else
-    CRES_INPUT=$CRES
-    FIXfv3_INPUT=$FIXfv3
-  fi
-    export CONVERT_NST=".true."
-    export INPUT_TYPE='restart'
-    export MOSAIC_FILE_INPUT_GRID="${FIXfv3_INPUT}/C${CRES_INPUT}_mosaic.nc"
-    export MOSAIC_FILE_TARGET_GRID="${FIXfv3}/C${CRES}_mosaic.nc"
-    export OROG_DIR_INPUT_GRID="${FIXfv3_INPUT}"
-
-    OROG_FILES_INPUT_GRID=""
-    ATM_CORE_FILES_INPUT=""
-    ATM_TRACER_FILES_INPUT=""
-    SFC_FILES_INPUT=""
-    for tile in {1..6}
-    do
-      OROG_FILES_INPUT_GRID=${OROG_FILES_INPUT_GRID}"C${CRES_INPUT}_oro_data.tile${tile}.nc"
-      ATM_CORE_FILES_INPUT=${ATM_CORE_FILES_INPUT}"${sPDY}.${scyc}0000.fv_core.res.tile${tile}.nc"
-      ATM_TRACER_FILES_INPUT=${ATM_TRACER_FILES_INPUT}"${sPDY}.${scyc}0000.fv_tracer.res.tile${tile}.nc"
-      SFC_FILES_INPUT=${SFC_FILES_INPUT}"${sPDY}.${scyc}0000.sfcanl_data.tile${tile}.nc"
-      if [[ $tile != 6 ]]; then
-        OROG_FILES_INPUT_GRID=${OROG_FILES_INPUT_GRID}'","'
-        ATM_CORE_FILES_INPUT=${ATM_CORE_FILES_INPUT}'","'
-        ATM_TRACER_FILES_INPUT=${ATM_TRACER_FILES_INPUT}'","'
-        SFC_FILES_INPUT=${SFC_FILES_INPUT}'","'
-      fi
-    done
-
-    export OROG_FILES_INPUT_GRID
-    ATM_CORE_FILES_INPUT=${ATM_CORE_FILES_INPUT}'","'
-    export ATM_CORE_FILES_INPUT=${ATM_CORE_FILES_INPUT}"${sPDY}.${scyc}0000.fv_core.res.nc"
-    export ATM_TRACER_FILES_INPUT
-    export SFC_FILES_INPUT
-
-    export TRACERS_TARGET='"sphum","liq_wat","o3mr","ice_wat","rainwat","snowwat","graupel"'
-    export TRACERS_INPUT='"sphum","liq_wat","o3mr","ice_wat","rainwat","snowwat","graupel"'
-
-    export COMIN=$INIDIR/RESTART
-
-    # Execute the script
-    $USHgfs/chgres_cube.sh
-    export err=$?
-    if [[ $err != 0 ]]; then
-      echo "FATAL ERROR in $(basename $BASH_SOURCE): chgres_cube failed!"
-      exit $err
-    fi
-  #fi
-
-  if [[ $SENDCOM == "YES" ]]; then
-    #if [[ $mem = c00 ]]; then
-      echo "Copying $mem to COM Directory!"
-      if [[ -e $COMOUT/INPUT ]]; then
-        rm -rf $COMOUT/INPUT
-      fi
-      mkdir -p $COMOUT/INPUT
-
-      for tile in {1..6}
-      do
-        mv $INIDIR/out.sfc.tile${tile}.nc $COMOUT/INPUT/sfc_data.tile${tile}.nc
-        mv $INIDIR/out.atm.tile${tile}.nc $COMOUT/INPUT/gfs_data.tile${tile}.nc
-      done
-      mv $INIDIR/gfs_ctrl.nc $COMOUT/INPUT/
-    #else
-    #  echo "Copying $mem to COM Directory!"
-    #  $NCP $INIDIR/*.nc $COMOUT/
-    #  $NCP $INIDIR/RESTART $COMOUT/
-    #fi
-  fi
-
-  echo "$(date -u) end $(basename $BASH_SOURCE)"
-  exit 0
-fi
-
-export INIDIR=$DATA
-export OUTDIR=$GESOUT/enkf/$mem
-INITDIR=$GESOUT/init/$mem
-mkdir -p $INIDIR
+#export INIDIR=$DATA
+export OUTDIR=${COMOUT}/$mem/atmos/INPUT
+#mkdir -p $INIDIR
 mkdir -p $OUTDIR
-mkdir -p $INITDIR
 
-cd $INIDIR
+#cd $INIDIR
+
+#mkdir $DATA
+cd $DATA
 
 if [[ $mem = c00 ]] ;then
   # Control intial conditions from current GFS cycle
-  ATMFILE=$COMINgfs/gfs.t${cyc}z.atmanl.nc
+  export ATM_FILES_INPUT="gfs.t${cyc}z.atmanl.nc"
+  ATMFILE=${COMINgfs}/atmos/${ATM_FILES_INPUT} #gfs.t${cyc}z.atmanl.nc
   if [[ -f $ATMFILE ]]; then
-    $NCP $ATMFILE $INIDIR
-    export ATM_FILES_INPUT="gfs.t${cyc}z.atmanl.nc"
+    $NCP $ATMFILE $DATA
+    #export ATM_FILES_INPUT="gfs.t${cyc}z.atmanl.nc"
   else
     msg="FATAL ERROR in $(basename $BASH_SOURCE): GFS atmospheric analysis file $ATMFILE not found!"
     echo "$msg"
@@ -233,11 +48,11 @@ else
     fi
 
     memchar="mem"$(printf %03i $cmem)
-    ATMFILE="$COMINenkf/$memchar/gdas.t${cycp}z.atmf006.nc"
+    export ATM_FILES_INPUT="gfs.t${cyc}z.ratmanl.nc"
+    ATMFILE="${COMINenkfgfs}/$memchar/atmos/${ATM_FILES_INPUT}"  #gfs.t${cycp}z.atmanl.nc"
 
     if [[ -f $ATMFILE ]]; then
-      $NCP $ATMFILE $INIDIR
-      export ATM_FILES_INPUT="gdas.t${cycp}z.atmf006.nc"
+      $NCP $ATMFILE $DATA
       success="YES"
 
     else
@@ -259,9 +74,9 @@ fi
 
 if [[ $CONVERT_SFC == ".true." ]]; then
   export SFC_FILES_INPUT="gfs.t${cyc}z.sfcanl.nc"
-  SFCFILE="$COMINgfs/$SFC_FILES_INPUT"
+  SFCFILE="$COMINgfs/atmos/$SFC_FILES_INPUT"
   if [[ -f $SFCFILE ]]; then
-    $NCP $SFCFILE $INIDIR
+    $NCP $SFCFILE $DATA
   else
     msg="FATAL ERROR in $(basename $BASH_SOURCE): GFS surfce analysis $SFCFILE not found!"
     echo $msg
@@ -271,10 +86,13 @@ if [[ $CONVERT_SFC == ".true." ]]; then
 fi
 
 export CRES=$(echo $CASE |cut -c2-5)
-export COMIN=$INIDIR
+export COMIN=$DATA
 export INPUT_TYPE="gaussian_netcdf"
 export FIXfv3=$FIXgfs/orog/C$CRES
 export FIXsfc=$FIXfv3/fix_sfc
+export FIXam=${FIXam:-$FIXgfs/am}
+export VCOORD_FILE=${VCOORD_FILE:-$FIXam/global_hyblev.l${LEVS}.txt}
+
 #############################################################
 # Execute the script
 $USHgfs/chgres_cube.sh
@@ -286,50 +104,57 @@ fi
 #############################################################
 
 # Move files to the nwges directory
-for tile in tile1 tile2 tile3 tile4 tile5 tile6; do
-  mv ${DATA}/out.atm.${tile}.nc $OUTDIR/gfs_data.${tile}.nc
-done
-mv ${DATA}/gfs_ctrl.nc $OUTDIR/.
+#for tile in tile1 tile2 tile3 tile4 tile5 tile6; do
+#  mv ${DATA}/out.atm.${tile}.nc $OUTDIR/gfs_data.${tile}.nc
+#done
+#mv ${DATA}/gfs_ctrl.nc $OUTDIR/.
 
 touch ${OUTDIR}/chgres_atm.log  # recenter can start now
 
-if [[ $CONVERT_SFC == ".true." ]]; then
-  # Copy sfc files to the nwges directory for all members
-  for mem2 in $memberlist; do
-    INITDIR2=$GESOUT/init/$mem2
-    mkdir -p $INITDIR2
-    for tile in tile1 tile2 tile3 tile4 tile5 tile6; do
-      $NCP ${DATA}/out.sfc.${tile}.nc $INITDIR2/sfc_data.${tile}.nc
-    done
-  done
-fi
+#if [[ $CONVERT_SFC == ".true." ]]; then
+#  # Copy sfc files to the nwges directory for all members
+#  for mem2 in $memberlist; do
+#    INITDIR2=${COMOUT}/${mem2}/atmos/INPUT #$GESOUT/init/$mem2
+#    mkdir -p $INITDIR2
+#    for tile in tile1 tile2 tile3 tile4 tile5 tile6; do
+#      $NCP ${DATA}/out.sfc.${tile}.nc $INITDIR2/sfc_data.${tile}.nc
+#    done
+#  done
+#fi
 
-# Copy control file to init
-$NCP $OUTDIR/gfs_ctrl.nc $INITDIR
+## Copy control file to init
+#$NCP $OUTDIR/gfs_ctrl.nc $INITDIR
 
 if [[ $SENDCOM == "YES" ]]; then
   MODCOM=$(echo ${NET}_${COMPONENT} | tr '[a-z]' '[A-Z]')
   DBNTYP=${MODCOM}_INIT
-  COMDIR=$COMOUT/init/$mem
+  COMDIR=${OUTDIR} #$COMOUT/$mem/atmos/INPUT #/init/$mem
   mkdir -p $COMDIR
-  $NCP $OUTDIR/gfs_ctrl.nc $COMDIR
+  $NCP ${DATA}/gfs_ctrl.nc $COMDIR
   if [[ $SENDDBN = YES ]];then
     $DBNROOT/bin/dbn_alert MODEL $DBNTYP $job $COMDIR/gfs_ctrl.nc
   fi
-  if [[ $mem == "c00" ]]; then
-    $NCP $OUTDIR/gfs_data*.nc $COMDIR
+  #if [[ $mem == "c00" ]]; then
+    #$NCP $OUTDIR/gfs_data*.nc $COMDIR
+    #for tile in tile1 tile2 tile3 tile4 tile5 tile6; do
+    #  mv ${DATA}/out.atm.${tile}.nc $OUTDIR/gfs_data.${tile}.nc
+    #done
+    #if [[ $SENDDBN = YES ]];then
+  for tile in tile1 tile2 tile3 tile4 tile5 tile6; do
+    $NCP ${DATA}/out.atm.${tile}.nc $OUTDIR/gfs_data.${tile}.nc
     if [[ $SENDDBN = YES ]];then
-      for tile in tile1 tile2 tile3 tile4 tile5 tile6; do
-        $DBNROOT/bin/dbn_alert MODEL $DBNTYP $job $COMDIR/gfs_data.${tile}.nc
-      done
+      $DBNROOT/bin/dbn_alert MODEL $DBNTYP $job $COMDIR/gfs_data.${tile}.nc
     fi
-  fi
+  done
+  #  fi
+  #fi
   if [[ $CONVERT_SFC == ".true." ]]; then
     for mem2 in $memberlist; do
-      COMDIR2=$COMOUT/init/$mem2
+      COMDIR2=$COMOUT/${mem2}/atmos/INPUT #init/$mem2
       mkdir -p $COMDIR2
       for tile in tile1 tile2 tile3 tile4 tile5 tile6; do
-        $NCP $GESOUT/init/$mem/sfc_data.${tile}.nc $COMDIR2
+        #$NCP $GESOUT/init/$mem/sfc_data.${tile}.nc $COMDIR2
+        $NCP ${DATA}/out.sfc.${tile}.nc $COMDIR2/sfc_data.${tile}.nc
         if [[ $SENDDBN = YES ]];then
           $DBNROOT/bin/dbn_alert MODEL $DBNTYP $job $COMDIR2/sfc_data.${tile}.nc
         fi
